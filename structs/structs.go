58f8b124478b0124d1e6fb144136b7328519930a
nickthorpe71 <nickthorpe71@gmail.com>
2021-01-09 07:43:51 -0600

    adds use of struct in a function

--- structs/structs.go
+++ structs/structs.go
@@ -2,13 +2,28 @@ package main
 
 import "fmt"
 
-type Position struct {
+type position struct {
 	x float32
 	y float32
 }
 
-func main() {
-	p := Position{3, 4}
+type badGuy struct {
+	name   string
+	health int
+	pos    position
+}
 
+func whereIsBadGuy(b badGuy) {
+	x := b.pos.x
+	y := b.pos.y
+	fmt.Println(x, y)
+}
+
+func main() {
+	p := position{3, 4}
 	fmt.Println(p.x)
+
+	b := badGuy{"mean person", 100, p}
+	fmt.Println(b.name)
+	whereIsBadGuy(b)
 }
