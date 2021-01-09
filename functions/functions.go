6165060e73ee60a24bbadbcbc00d5b6b6066446c
nickthorpe71 <nickthorpe71@gmail.com>
2021-01-09 07:30:45 -0600

    adds return type example

--- functions/functions.go
+++ functions/functions.go
@@ -6,6 +6,24 @@ func sayHello(name string) {
 	fmt.Println("hi,", name)
 }
 
+func saySomething() {
+	fmt.Println("something... ")
+}
+
+func beSocial(name string) {
+	sayHello(name)
+	saySomething()
+}
+
+func addOne(x int) int {
+	return x + 1
+}
+
 func main() {
-	sayHello("human")
+	beSocial("human1")
+	beSocial("human2")
+
+	x := 5
+	x = addOne(x)
+	fmt.Println(x)
 }
