57a3fc72548b781a6fbea15b81f2bdf3b2d22496
nickthorpe71 <nickthorpe71@gmail.com>
2021-01-09 07:53:07 -0600

    adds pointer function parameter example

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+func addOne(x int) {
+	x = x + 1
+}
+
+func mutatePlusOne(x *int) { // take pointer as a parameter
+	*x = *x + 1
+}
+
 func main() {
 
 	x := 5
@@ -10,4 +18,10 @@ func main() {
 	// xPtr := &x
 	var xPtr *int = &x
 	fmt.Println(xPtr)
+
+	addOne(x)      // sends a copy of x
+	fmt.Println(x) // so x remains 5
+
+	mutatePlusOne(xPtr)
+	fmt.Println(x)
 }
