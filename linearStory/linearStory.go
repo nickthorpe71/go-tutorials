32dc2a55cb4efb88490a402c51cf642c9f9093c0
nickthorpe71 <nickthorpe71@gmail.com>
2021-01-09 08:05:17 -0600

    adds 3 pages with pointers to eachother

--- linearStory/linearStory.go
+++ linearStory/linearStory.go
@@ -13,5 +13,14 @@ type storyPage struct {
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+
+	page1 := storyPage{"It was a hot day in Chicago.", nil}
+	page2 := storyPage{"You are waiting in line at the veggie hotdog stand.", nil}
+	page3 := storyPage{"Someone in the font of the line is complaining that there is not enough dog in their veggiedog....", nil}
+
+	page1.nextPage = &page2
+	page2.nextPage = &page3
+
 	fmt.Println(scanner)
+
 }
