948eb1a020fa724de0453b6bdbb47041b3f88c8d
nickthorpe71 <nickthorpe71@gmail.com>
2021-01-10 11:10:54 -0600

    adds node struct and play function

--- branchingStory/branchingStory.go
+++ branchingStory/branchingStory.go
@@ -1,5 +1,43 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+type storyNode struct {
+	text    string
+	yesPath *storyNode
+	noPath  *storyNode
+}
+
+func (node *storyNode) play() {
+	fmt.Println(node.text)
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		scanner.Scan()
+		answer := scanner.Text()
+
+		if answer == "yes" {
+			node.yesPath.play()
+			break
+		} else if answer == "no" {
+			node.noPath.play()
+			break
+		} else {
+			fmt.Println("must answer 'yes' or 'no'")
+		}
+	}
+
+}
+
+func main() {
+	root := storyNode{"You wake up in a desert oasis and you see a grotesque beast guarding the entrance to a cave. Do you want to approach the cave?", nil, nil}
+	winning := storyNode{"You move along.", nil, nil}
+	losing := storyNode{"In a deep gritty female voice the creature calls out in a language you cannot understand. Faster than you can blink or react the creature has one of their pale fingered feet wrapped arount your head and begins removing your limbs with the others... RIP", nil, nil}
+
+	root.yesPath = &losing
+	root.noPath = &winning
 }
