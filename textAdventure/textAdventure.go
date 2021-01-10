cc079b46d0b97098c173e5f34be6128fa4d31283
nickthorpe71 <nickthorpe71@gmail.com>
2021-01-10 11:43:02 -0600

    adds linked list and graph nodes

--- textAdventure/textAdventure.go
+++ textAdventure/textAdventure.go
@@ -1,5 +1,19 @@
 package main
 
+// linked list
+type choices struct {
+	cmd         string
+	description string
+	node        *storyNode
+	next        *choices
+}
+
+// graph
+type storyNode struct {
+	text    string
+	choices *choices
+}
+
 func main() {
 
 }
