542785b52e3bd6345956d9994fef1c8225c6ceb8
nickthorpe71 <nickthorpe71@gmail.com>
2021-01-10 11:49:07 -0600

    adds addCHoice function

--- textAdventure/textAdventure.go
+++ textAdventure/textAdventure.go
@@ -4,14 +4,28 @@ package main
 type choices struct {
 	cmd         string
 	description string
-	node        *storyNode
-	next        *choices
+	nextNode    *storyNode
+	nextChoice  *choices
 }
 
 // graph
 type storyNode struct {
 	text    string
 	choices *choices
+}
+
+func (node *storyNode) addChoice(cmd string, description string, nextNode *storyNode) {
+	choice := &choices{cmd, description, nextNode, nil}
+
+	if node.choices == nil {
+		node.choices = choice
+	} else {
+		currentChoice := node.choices
+		for currentChoice.nextChoice != nil {
+			currentChoice = currentChoice.nextChoice
+		}
+		currentChoice.nextChoice = choice
+	}
 }
 
 func main() {
