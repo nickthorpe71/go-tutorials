373a4150cbc1203ba63df74bdbb86641aef516ba
nickthorpe71 <nickthorpe71@gmail.com>
2021-01-20 06:13:58 -0600

    completes fibinacci memoized

--- fib/fib.go
+++ fib/fib.go
@@ -26,8 +26,31 @@ func fibRecursive(n uint) uint {
 	}
 }
 
+func fibDynamic(n int, memo []int) int {
+	if memo[n] != 0 {
+		fmt.Println("memo!")
+		return memo[n]
+	}
+	if n == 0 {
+		return 0
+	} else if n == 1 {
+		return 1
+	} else {
+		result := fibDynamic(n-1, memo) + fibDynamic(n-2, memo)
+		memo[n] = result
+		return result
+	}
+}
+
 func main() {
 	fmt.Println(fibIterative(5)) // iterative
 	fmt.Println(fibRecursive(5)) // recursive
+
+	memo := make([]int, 5+1)
+	fmt.Println(fibDynamic(5, memo)) // memoized
+
 	// write dynamic version
+	// bottom up version
+
+	// https://www.youtube.com/watch?v=vYquumk4nWw&ab_channel=CSDojo
 }
