46f3f76e1ce43d9774b2a565572f1a80d36baaa9
nickthorpe71 <nickthorpe71@gmail.com>
2021-02-09 10:00:30 -0600

    adds basic read write example

--- microservices/main.go
+++ microservices/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -9,8 +11,14 @@ import (
 // https://www.youtube.com/watch?v=VzBGi_n65iU&ab_channel=NicJackson
 
 func main() {
-	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		log.Println("Hello Internet")
+		data, _ := ioutil.ReadAll(r.Body)
+		fmt.Fprintf(rw, "Hello %s\n", data)
+	})
+
+	http.HandleFunc("/north", func(http.ResponseWriter, *http.Request) {
+		log.Println("after heading north you see a cliff")
 	})
 
 	http.ListenAndServe(":9090", nil)
