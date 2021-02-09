9d31f63ac06b566b43cdce1c950896b4d4a97d1c
nickthorpe71 <nickthorpe71@gmail.com>
2021-02-09 06:08:46 -0600

    adds basic endpoint

--- microservices/main.go
+++ microservices/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+// Tutorial
+// https://www.youtube.com/watch?v=VzBGi_n65iU&ab_channel=NicJackson
+
+func main() {
+	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
+		log.Println("Hello Internet")
+	})
+
+	http.ListenAndServe(":9090", nil)
 }
